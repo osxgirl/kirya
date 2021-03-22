5681a545589adc6e9fa0d4c65fc48ec2968dc627
osxgirl <osxgirl@mac.com>
2021-03-22 16:48:53 -0400

    + db + CSS

---
+++ kriya-backend/db/migrate/20210322204440_videos.rb
@@ -0,0 +1,7 @@
+class Videos < ActiveRecord::Migration[5.2]
+  def change
+      create_table :videos do |t|
+              t.string :name
+   end
+  end
+end
--- kriya-backend/db/schema.rb
+++ kriya-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_22_202330) do
+ActiveRecord::Schema.define(version: 2021_03_22_204440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,10 @@ ActiveRecord::Schema.define(version: 2021_03_22_202330) do
     t.string "email"
     t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
+  create_table "videos", force: :cascade do |t|
+    t.string "name"
   end
 
 end
