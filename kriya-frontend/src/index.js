bf87aac48ea7edfa94333b2b0966491ba7d314e3
osxgirl <osxgirl@mac.com>
2021-04-02 18:34:15 -0400

    Images DOM JSON

--- kriya-frontend/src/index.js
+++ kriya-frontend/src/index.js
@@ -1,7 +1,22 @@
+
 console.log("testing...")
 // test that we can get data from the backend
-
 const BACKEND_URL = 'http://localhost:3000';
 fetch(`${BACKEND_URL}/test`)
   .then(response => response.json())
   .then(parsedResponse => console.log(parsedResponse));
+
+fetch(`${BACKEND_URL}/index`)
+  .then(response => response.json())
+  .then(parsedResponse => console.log(parsedResponse));
+
+//images
+function fetchImages() {
+  return fetch(`${BACKEND_URL}/show`)
+    .then(response => response.json())
+};
+document.addEventListener('DOMContentLoaded', () => {
+  fetchImages()
+    .then(results => console.log(results));
+});
+
