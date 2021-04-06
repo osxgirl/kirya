5913ba9e5a8941dac34ceda8c22495a3681c0255
osxgirl <osxgirl@mac.com>
2021-04-06 10:04:11 -0400

    DOM Masters🕉

--- kriya-frontend/src/index.js
+++ kriya-frontend/src/index.js
@@ -1,4 +1,3 @@
-
 console.log("testing...")
 // test that we can get data from the backend
 const BACKEND_URL = 'http://localhost:3000';
@@ -12,11 +11,27 @@ fetch(`${BACKEND_URL}/index`)
 
 //images
 function fetchImages() {
-  return fetch(`${BACKEND_URL}/show`)
+  return fetch(`${BACKEND_URL}/images`)
     .then(response => response.json())
 };
 document.addEventListener('DOMContentLoaded', () => {
   fetchImages()
     .then(results => console.log(results));
+    
+    var p = document.createElement('p');
+    p.innerHTML = 'Babaji • Lahiri • Jesus • Yukteswar • Paramahansa';
+//    image.style.backgroundColor = '#f9f9f9';
+    document.body.appendChild(p);
+
 });
+
+//space people
+fetch('http://api.open-notify.org/astros.json')
+.then(function(response) {
+  return response.json();
+}).then(function(json) {
+  console.log(json);
+  console.log(`Holy cow! There are ${json["number"]} humans in space.`);
+});
+
 
