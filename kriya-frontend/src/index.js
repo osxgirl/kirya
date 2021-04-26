dd877b629e3f2fbb10fea8ac1bf831cf7ff3da32
osxgirl <osxgirl@mac.com>
2021-04-25 21:01:28 -0400

    like-glyph ❤️  + Master images

--- kriya-frontend/src/index.js
+++ kriya-frontend/src/index.js
@@ -1,11 +1,52 @@
 console.log("connected")
 
+let glyphStates = {
+  "♡": "♥",
+  "♥": "♡"
+};
+
+let colorStates = {
+  "red" : "",
+  "": "red"
+};
+
+let articleHearts = document.querySelectorAll(".like-glyph");
+
+function likeCallback(e) {
+  let heart = e.target;
+  mimicServerCall()
+    .then(function(serverMessage){
+  
+      alert("You notified the server!");
+      alert(serverMessage);
+      heart.innerText = glyphStates[heart.innerText];
+      heart.style.color = colorStates[heart.style.color];
+    })
+    .catch(function(error) {
+      alert("Something went wrong!");
+    });
+}
+
+for (let glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
+
+
+function mimicServerCall() {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      resolve("Pretend remote server notified of action!");
+    }, 300);
+  });
+}
+
 const BACKEND_URL = 'http://localhost:3000';
 //images
 function fetchImages() {
   return fetch(`${BACKEND_URL}/images`)
     .then(response => response.json())
 };
+
 
 document.addEventListener('DOMContentLoaded', () => {
   fetchImages()
@@ -14,7 +55,31 @@ document.addEventListener('DOMContentLoaded', () => {
     var p = document.createElement('p');
     p.innerHTML = 'Babaji • Lahiri • Jesus • Yukteswar • Paramahansa';
     document.body.appendChild(p);
+    
+
+        var p2 = document.createElement('p2');
+        p2.innerHTML="<img src=\"images/Babaji.jpg\">";
+        document.body.appendChild(p2);
+        
+        var p2 = document.createElement('p2');
+        p2.innerHTML="<img src=\"images/Lahiri+original+photo.jpg\">";
+        document.body.appendChild(p2);
+    
+        var p2 = document.createElement('p2');
+        p2.innerHTML="<img src=\"images/Jesus+Heather+small.jpg\">";
+        document.body.appendChild(p2);
+    
+        var p2 = document.createElement('p2');
+        p2.innerHTML="<img src=\"images/Yukteswar+Heather.jpg\">";
+        document.body.appendChild(p2);
+    
+        var p2 = document.createElement('p2');
+        p2.innerHTML="<img src=\"images/Yogananda+Ananda+Temple.jpg\">";
+        document.body.appendChild(p2);
+
 });
+
+
 
 function fetchVideos() {
   return fetch(`${BACKEND_URL}/videos`)
@@ -25,6 +90,9 @@ document.addEventListener('DOMContentLoaded', () => {
     .then(results => console.log(results));
     
 });
+
+
+
 
 fetch('http://api.open-notify.org/astros.json')
 .then(function(response) {
@@ -53,3 +121,5 @@ function createQuote(event) {
     
   
 };
+
+
